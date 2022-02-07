cad42ec59cd8f525dd206e5f5c45dcaf30c7c0de
Spencer Knight <spencer.knightusn@gmail.com>
2022-02-06 17:37:53 -0700

    installed passport / google auth

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const debug = require('debug')('passport-learning:users');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
+  debug(req.cookies);
   res.send('respond with a resource');
 });
 
